08567b6844d1e7037b0b8aa2d4ee2d50422470d2
siddarth.msv <82795818+Sidddddarth@users.noreply.github.com>
2023-04-19 11:41:54 +0530

    Update ro/ro.go
    Co-authored-by: Aris Tzoumas <atzoumas@rudderstack.com>

--- ro/ro.go
+++ ro/ro.go
@@ -1,5 +1,6 @@
 package ro
 
+// Memoize stores the execution result of the provided function in-memory during the first call and uses it as a return value for subsequent calls
 func Memoize[R any](f func() R) func() R {
 	var result R
 	var called bool
