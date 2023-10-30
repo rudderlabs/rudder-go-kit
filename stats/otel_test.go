5176c95f6b8dbb4e3a413db74380b25c3b30e9d7
Francesco Casula <fra.casula@gmail.com>
2023-10-30 11:13:39 +0100

    chore: test invalid instruments

--- stats/otel_test.go
+++ stats/otel_test.go
@@ -20,6 +20,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	otelMetric "go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/metric/noop"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"go.opentelemetry.io/otel/sdk/resource"
@@ -849,6 +850,53 @@ func TestPrometheusDuplicatedAttributes(t *testing.T) {
 	), metrics[metricName].Metric[0].Label, "Got %+v", metrics[metricName].Metric[0].Label)
 }
 
+func TestInvalidInstrument(t *testing.T) {
+	newStats := func(t *testing.T, match string) *otelStats {
+		ctrl := gomock.NewController(t)
+		l := mock_logger.NewMockLogger(ctrl)
+		l.EXPECT().Warnf(&containsMatcher{value: match}, gomock.Any()).Times(1)
+
+		enabled := atomic.Bool{}
+		enabled.Store(true)
+
+		return &otelStats{
+			config:    statsConfig{enabled: &enabled},
+			meter:     sdkmetric.NewMeterProvider().Meter(""),
+			noopMeter: noop.NewMeterProvider().Meter(""),
+			logger:    l,
+		}
+	}
+
+	t.Run("counter", func(t *testing.T) {
+		s := newStats(t, "failed to create instrument")
+		require.NotPanics(t, func() {
+			m := s.getMeasurement("_#@!?", CountType, nil)
+			m.Increment()
+		})
+	})
+	t.Run("gauge", func(t *testing.T) {
+		s := newStats(t, "failed to create gauge")
+		require.NotPanics(t, func() {
+			m := s.getMeasurement("_#@!?", GaugeType, nil)
+			m.Gauge(123)
+		})
+	})
+	t.Run("timer", func(t *testing.T) {
+		s := newStats(t, "failed to create instrument")
+		require.NotPanics(t, func() {
+			m := s.getMeasurement("_#@!?", TimerType, nil)
+			m.SendTiming(123 * time.Millisecond)
+		})
+	})
+	t.Run("histogram", func(t *testing.T) {
+		s := newStats(t, "failed to create instrument")
+		require.NotPanics(t, func() {
+			m := s.getMeasurement("_#@!?", HistogramType, nil)
+			m.Observe(123)
+		})
+	})
+}
+
 func getDataPoint[T any](ctx context.Context, t *testing.T, rdr sdkmetric.Reader, name string, idx int) (zero T) {
 	t.Helper()
 	rm := metricdata.ResourceMetrics{}
@@ -951,4 +999,13 @@ func (f loggerSpyFactory) NewLogger() logger.Logger { return f.spy }
 
 func newLoggerSpyFactory(l logger.Logger) loggerFactory {
 	return &loggerSpyFactory{spy: l}
+}
+
+type containsMatcher struct {
+	value string
+}
+
+func (m containsMatcher) String() string { return m.value }
+func (m containsMatcher) Matches(arg interface{}) bool {
+	return strings.Contains(arg.(string), m.value)
 }
