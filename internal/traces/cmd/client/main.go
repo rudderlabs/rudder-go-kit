eb0a94995437856f6108d3bc78be89a5eb9c8d89
Francesco Casula <fra.casula@gmail.com>
2023-10-31 08:33:13 +0100

    chore: client cleanup

--- internal/traces/cmd/client/main.go
+++ internal/traces/cmd/client/main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httptrace"
-	"time"
-
-	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	"os"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/httptrace/otelhttptrace"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
@@ -18,19 +15,22 @@ import (
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
 const serverURL = "http://localhost:7777/hello"
 
 func main() {
+	logger := log.New(os.Stderr, "client", log.Ldate|log.Ltime|log.Llongfile)
+
 	tp, err := initTracer()
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 	defer func() {
 		if err := tp.Shutdown(context.Background()); err != nil {
-			log.Printf("Error shutting down tracer provider: %v", err)
+			logger.Printf("Error shutting down tracer provider: %v", err)
 		}
 	}()
 
@@ -55,9 +55,9 @@ func main() {
 		defer span.End()
 
 		ctx = httptrace.WithClientTrace(ctx, otelhttptrace.NewClientTrace(ctx))
-		req, _ := http.NewRequestWithContext(ctx, "GET", "http://localhost:7777/hello", nil)
+		req, _ := http.NewRequestWithContext(ctx, "GET", serverURL, nil)
 
-		fmt.Printf("Sending request...\n")
+		logger.Println("Sending request...")
 		res, err := client.Do(req)
 		if err != nil {
 			panic(err)
@@ -72,10 +72,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Response Received: %s\n\n\n", body)
-	fmt.Printf("Waiting for few seconds to export spans ...\n\n")
-	time.Sleep(3 * time.Second)
-	fmt.Printf("Inspect traces on stdout\n")
+	logger.Printf("Response Received: %s", body)
 }
 
 func initTracer() (*sdktrace.TracerProvider, error) {
