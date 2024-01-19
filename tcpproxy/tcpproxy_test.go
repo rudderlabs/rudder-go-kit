b4d316760be2678fdce05a48c54ff54d8103a06e
Francesco Casula <fra.casula@gmail.com>
2024-01-19 10:56:06 +0100

    chore: tcp proxy test

---
+++ tcpproxy/tcpproxy_test.go
@@ -0,0 +1,51 @@
+package tcpproxy
+
+import (
+	"io"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/rudderlabs/rudder-go-kit/testhelper"
+)
+
+func TestProxy(t *testing.T) {
+	httpServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello, world!"))
+	}))
+	defer httpServer.Close()
+
+	httpServerParsedURL, err := url.Parse(httpServer.URL)
+	require.NoError(t, err)
+
+	_, httpServerPort, err := net.SplitHostPort(httpServerParsedURL.Host)
+	require.NoError(t, err)
+
+	proxyPort, err := testhelper.GetFreePort()
+	require.NoError(t, err)
+
+	proxy := &Proxy{
+		LocalAddr:  "localhost:" + strconv.Itoa(proxyPort),
+		RemoteAddr: "localhost:" + httpServerPort,
+	}
+	go proxy.Start(t)
+	t.Cleanup(proxy.Stop)
+
+	var resp *http.Response
+	require.Eventually(t, func() bool {
+		resp, err = http.Get("http://" + proxy.LocalAddr)
+		return err == nil
+	}, 5*time.Second, 10*time.Millisecond, "failed to connect to proxy")
+
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	require.Equal(t, "Hello, world!", string(body))
+}
