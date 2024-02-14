03c5d49d47ee36bd52359829122969c800bdf755
mihir <mihir20121997@gmail.com>
2024-02-14 19:39:14 +0530

    addressed comments

--- testhelper/docker/resource/transformer/transformer_test.go
+++ testhelper/docker/resource/transformer/transformer_test.go
@@ -1,8 +1,6 @@
 package transformer_test
 
 import (
-	"bytes"
-	"io"
 	"net/http"
 	"net/url"
 	"testing"
@@ -47,7 +45,7 @@ func TestSetup(t *testing.T) {
 		for _, tt := range tests {
 			t.Run(tt.name, func(t *testing.T) {
 				transformerContainer, err := transformer.Setup(pool, t,
-					transformer.WithConfigBackendURL("https://api.dev.rudderlabs.com"),
+					transformer.WithConfigBackendURL("random-url"),
 					transformer.WithDockerImageTag(tt.tag))
 				require.NoError(t, err)
 				endpoint, err := url.JoinPath(transformerContainer.TransformerURL, tt.urlPath)
@@ -58,46 +56,5 @@ func TestSetup(t *testing.T) {
 				require.Equal(t, resp.StatusCode, http.StatusOK)
 			})
 		}
-	})
-
-	t.Run("user transformation", func(t *testing.T) {
-		transformerContainer, err := transformer.Setup(pool, t,
-			transformer.WithConfigBackendURL("https://api.dev.rudderlabs.com"),
-			transformer.WithDockerImageTag("ut-latest"))
-		require.NoError(t, err)
-
-		transformerURL, err := url.JoinPath(transformerContainer.TransformerURL, "customTransform")
-		require.NoError(t, err)
-
-		rawReq := []byte(`[{"message":{
-				"userId": "identified_user_id",
-				"anonymousId":"anonymousId_1",
-				"messageId":"messageId_1",
-				"type": "track",
-				"event": "Product Reviewed",
-				"properties": {
-				  "review_id": "12345",
-				  "product_id" : "123",
-				  "rating" : 3.5,
-				  "review_body" : "Average product, expected much more."
-				}
-			},"metadata":{"sourceId":"xxxyyyzzEaEurW247ad9WYZLUyk","workspaceId":"fyJvxaglFrCFxsBcLiSPBCmgpWK",
-			"messageId":"messageId_1"},"destination":{"Transformations":[{"VersionID":"2Nazu8t4ujUC0Dzc4pBFbjmOijx","ID":""}]}}]`)
-		req, reqErr := http.NewRequest(http.MethodPost, transformerURL, bytes.NewBuffer(rawReq))
-		if reqErr != nil {
-			return
-		}
-		req.Header.Set("Content-Type", "application/json; charset=utf-8")
-		req.Header.Set("X-Feature-Gzip-Support", "?1")
-		// Header to let transformer know that the client understands event filter code
-		req.Header.Set("X-Feature-Filter-Code", "?1")
-
-		resp, err := http.DefaultClient.Do(req)
-		require.NoError(t, err)
-		defer func() { _ = resp.Body.Close() }()
-		require.Equal(t, resp.StatusCode, http.StatusOK)
-		respData, err := io.ReadAll(resp.Body)
-		require.NoError(t, err)
-		require.Contains(t, string(respData), `"transformed":true`)
 	})
 }
