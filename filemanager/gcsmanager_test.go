b5cd2e9e497108d1b67092a61e9900e0da1df426
Leonidas Vrachnis <leo.al.vra@gmail.com>
2024-05-27 07:56:48 +0200

    chore: rewrite tests to use fakestorage (#483)

--- filemanager/gcsmanager_test.go
+++ filemanager/gcsmanager_test.go
@@ -2,45 +2,100 @@ package filemanager
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/rudderlabs/rudder-go-kit/config"
 	"github.com/rudderlabs/rudder-go-kit/logger"
-	"github.com/stretchr/testify/require"
+	"github.com/rudderlabs/rudder-go-kit/testhelper"
+
+	"github.com/fsouza/fake-gcs-server/fakestorage"
 )
 
 func TestGCSManagerOpts(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	c := config.New()
 
-	conf := map[string]interface{}{
-		"bucketName":  "test-bucket",
-		"prefix":      "test-prefix",
-		"credentials": c.GetString("GCS_TEST_CREDENTIALS", "<credentials>"),
-		"disableSSL":  true,
-		"jsonReads":   true,
+	// testcases:
+	tcs := []struct {
+		Name                string
+		GCSUploadIfNotExist bool
+	}{
+		{
+			Name:                "without UploadIfNotExist",
+			GCSUploadIfNotExist: true,
+		},
+		{
+			Name:                "with UploadIfNotExist",
+			GCSUploadIfNotExist: false,
+		},
 	}
-	settings := &Settings{
-		Provider:            "GCS",
-		Config:              conf,
-		Logger:              logger.NOP,
-		Conf:                config.New(),
-		GCSUploadIfNotExist: true,
+
+	for _, tc := range tcs {
+		t.Run(tc.Name, func(t *testing.T) {
+			port, err := testhelper.GetFreePort()
+			require.NoError(t, err)
+
+			server, err := fakestorage.NewServerWithOptions(fakestorage.Options{
+				InitialObjects: []fakestorage.Object{
+					{
+						ObjectAttrs: fakestorage.ObjectAttrs{
+							BucketName: "test-bucket",
+							Name:       "test-prefix/testFile",
+						},
+						Content: []byte("inside the file"),
+					},
+				},
+				Scheme: "http",
+				Host:   "127.0.0.1",
+				Port:   uint16(port),
+			})
+			require.NoError(t, err)
+			defer server.Stop()
+
+			gcsURL := fmt.Sprintf("%s/storage/v1/", server.URL())
+			t.Log("GCS URL:", gcsURL)
+
+			conf := map[string]interface{}{
+				"bucketName": "test-bucket",
+				"prefix":     "test-prefix",
+				"endPoint":   gcsURL,
+				"disableSSL": true,
+				"jsonReads":  true,
+			}
+			m, err := New(&Settings{
+				Provider:            "GCS",
+				Config:              conf,
+				Logger:              logger.NOP,
+				Conf:                config.New(),
+				GCSUploadIfNotExist: tc.GCSUploadIfNotExist,
+			})
+			require.NoError(t, err)
+
+			tempDir := t.TempDir()
+			f, err := os.Create(tempDir + "/testFile")
+			require.NoError(t, err)
+
+			t.Log("pre-existing file")
+			uploadedFile, err := m.Upload(ctx, f)
+			if tc.GCSUploadIfNotExist {
+				require.Equal(t, UploadedFile{}, uploadedFile)
+				require.ErrorIs(t, err, ErrPreConditionFailed)
+			} else {
+				require.NoError(t, err)
+			}
+
+			t.Run("new file", func(t *testing.T) {
+				tempDir := t.TempDir()
+				f, err := os.Create(tempDir + "/testFile-new")
+				require.NoError(t, err)
+
+				_, err = m.Upload(ctx, f)
+				require.NoError(t, err)
+			})
+		})
 	}
-
-	m, err := New(settings)
-	require.NoError(t, err)
-
-	tempDir := t.TempDir()
-	f, err := os.Create(tempDir + "/testFile")
-	require.NoError(t, err)
-	uploadedFile, err := m.Upload(ctx, f)
-	require.NoError(t, err)
-	require.Equal(t, "testFile", uploadedFile.ObjectName)
-
-	uploadedFile, err = m.Upload(ctx, f)
-	require.Equal(t, UploadedFile{}, uploadedFile)
-	require.ErrorIs(t, err, ErrPreConditionFailed)
 }
