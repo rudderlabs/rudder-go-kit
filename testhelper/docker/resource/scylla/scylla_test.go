9d53cbd182a09ffb4216cee38103b5e735224b4e
Rohith BCS <rohith.bcs@gmail.com>
2024-08-05 12:12:01 +0530

    chore: add test with keyspace

--- testhelper/docker/resource/scylla/scylla_test.go
+++ testhelper/docker/resource/scylla/scylla_test.go
@@ -23,3 +23,19 @@ func TestScylla(t *testing.T) {
 	require.NotNil(t, session)
 	session.Close()
 }
+
+func TestScylla_WithKeySpace(t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	scyllaContainer, err := Setup(pool, t, WithKeyspace("test"))
+	require.NoError(t, err)
+	require.NotNil(t, scyllaContainer)
+
+	cluster := gocql.NewCluster(scyllaContainer.URL)
+	cluster.Consistency = gocql.Quorum
+	session, err := cluster.CreateSession()
+	require.NoError(t, err)
+	require.NotNil(t, session)
+	session.Close()
+}
