091c7ded0c5fe17f63c599acb5cbf681ced7bbd0
Aris Tzoumas <atzoumas@rudderstack.com>
2024-08-30 13:38:09 +0300

    fix: docker containers listening on ipv6

--- testhelper/docker/resource/internal/ports.go
+++ testhelper/docker/resource/internal/ports.go
@@ -1,22 +1,35 @@
 package internal
 
 import (
+	"runtime"
+
 	"github.com/ory/dockertest/v3/docker"
 )
 
-const BindHostIP = "0.0.0.0"
+const (
+	BindHostIP       = "127.0.0.1"
+	BindInternalHost = "host.docker.internal"
+)
 
 // IPv4PortBindings returns the port bindings for the given exposed ports forcing ipv4 address.
 func IPv4PortBindings(exposedPorts []string) map[docker.Port][]docker.PortBinding {
 	portBindings := make(map[docker.Port][]docker.PortBinding)
 
+	bindings := []docker.PortBinding{
+		{
+			HostIP:   BindHostIP,
+			HostPort: "0",
+		},
+	}
+	if runtime.GOOS == "linux" {
+		bindings = append(bindings, docker.PortBinding{
+			HostIP:   BindInternalHost,
+			HostPort: "0",
+		})
+	}
+
 	for _, exposedPort := range exposedPorts {
-		portBindings[docker.Port(exposedPort)+"/tcp"] = []docker.PortBinding{
-			{
-				HostIP:   BindHostIP,
-				HostPort: "0",
-			},
-		}
+		portBindings[docker.Port(exposedPort)+"/tcp"] = bindings
 	}
 
 	return portBindings
