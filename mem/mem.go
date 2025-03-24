c078f83e50561d3688832c0c0faaa9d15c3c5651
Aris Tzoumas <atzoumas@rudderstack.com>
2025-03-24 22:32:04 +0200

    feat: include rss memory in calculated mem stats

--- mem/mem.go
+++ mem/mem.go
@@ -12,14 +12,31 @@ import (
 type Stat struct {
 	// Total memory in bytes
 	Total uint64
-	// Available memory in bytes
+	// Available memory in bytes, calculated as [Total] - [Used]
 	Available uint64
 	// Available memory in percentage
 	AvailablePercent float64
-	// Used memory in bytes
+	// Used memory in bytes. Includes [total_active_file], that is cache memory that has been identified as active by the kernel.
 	Used uint64
 	// Used memory in percentage
 	UsedPercent float64
+	// Size of RSS in bytes. Like Used, but without [total_active_file].
+	RSS uint64
+}
+
+// RSSPercent returns the percentage of RSS memory
+func (s *Stat) RSSPercent() float64 {
+	return float64(s.RSS) * 100 / float64(s.Total)
+}
+
+// AvailableIgnoreCache returns the available memory in bytes, ignoring cache memory. This is calculated as [Total] - [RSS]
+func (s *Stat) AvailableIgnoreCache() uint64 {
+	return s.Total - s.RSS
+}
+
+// AvailableIgnoreCachePercent returns the available memory in percentage, ignoring cache memory. This is calculated as [Total] - [RSS]
+func (s *Stat) AvailableIgnoreCachePercent() float64 {
+	return float64(s.AvailableIgnoreCache()) * 100 / float64(s.Total)
 }
 
 // Get current memory statistics
@@ -52,11 +69,16 @@ func (c *collector) Get() (*Stat, error) {
 		if stat.Used > stat.Total {
 			stat.Used = stat.Total
 		}
+		stat.RSS = uint64(cgroup.GetMemoryRSS(c.basePath))
+		if stat.RSS == 0 || stat.RSS > stat.Used {
+			stat.RSS = stat.Used
+		}
 		stat.Available = stat.Total - stat.Used
 	} else {
 		stat.Total = mem.Total
 		stat.Available = mem.Available
 		stat.Used = stat.Total - stat.Available
+		stat.RSS = stat.Used
 	}
 	stat.AvailablePercent = float64(stat.Available) * 100 / float64(stat.Total)
 	stat.UsedPercent = float64(stat.Used) * 100 / float64(stat.Total)
