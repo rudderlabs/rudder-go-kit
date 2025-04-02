c3be2a2f1fa4bf10fca1607d54825cb999e6df3e
Francesco Casula <fra.casula@gmail.com>
2025-04-02 14:23:34 +0200

    chore: use NOPTracer always

--- stats/nop.go
+++ stats/nop.go
@@ -3,8 +3,6 @@ package stats
 import (
 	"context"
 	"time"
-
-	"go.opentelemetry.io/otel/trace/noop"
 )
 
 var (
@@ -36,9 +34,7 @@ func (*nop) NewSampledTaggedStat(_, _ string, _ Tags) Measurement {
 	return &nopMeasurement{}
 }
 
-func (*nop) NewTracer(_ string) Tracer {
-	return NewTracerFromOpenTelemetry(noop.NewTracerProvider().Tracer(""))
-}
+func (*nop) NewTracer(_ string) Tracer { return NOPTracer }
 
 func (*nop) Start(_ context.Context, _ GoRoutineFactory) error { return nil }
 func (*nop) Stop()                                             {}
