fce55029c7c1087f0568ba4628a0b878052a1a99
Francesco Casula <fracasula@users.noreply.github.com>
2025-04-02 14:51:45 +0200

    chore: nop tracer (#769)

--- stats/nop.go
+++ stats/nop.go
@@ -3,11 +3,12 @@ package stats
 import (
 	"context"
 	"time"
-
-	"go.opentelemetry.io/otel/trace/noop"
 )
 
-var NOP Stats = &nop{}
+var (
+	NOP       Stats  = &nop{}
+	NOPTracer Tracer = &nopTracer{}
+)
 
 type nop struct{}
 
@@ -33,11 +34,27 @@ func (*nop) NewSampledTaggedStat(_, _ string, _ Tags) Measurement {
 	return &nopMeasurement{}
 }
 
-func (*nop) NewTracer(_ string) Tracer {
-	return NewTracerFromOpenTelemetry(noop.NewTracerProvider().Tracer(""))
-}
+func (*nop) NewTracer(_ string) Tracer { return NOPTracer }
 
 func (*nop) Start(_ context.Context, _ GoRoutineFactory) error { return nil }
 func (*nop) Stop()                                             {}
 
 func (*nop) RegisterCollector(c Collector) error { return nil }
+
+type nopTracer struct{}
+
+func (*nopTracer) Start(ctx context.Context, _ string, _ SpanKind, _ ...SpanOption) (context.Context, TraceSpan) {
+	return ctx, &nopSpan{}
+}
+
+func (*nopTracer) SpanFromContext(_ context.Context) TraceSpan {
+	return &nopSpan{}
+}
+
+type nopSpan struct{}
+
+func (*nopSpan) AddEvent(_ string, _ ...SpanOption) {}
+func (*nopSpan) SetStatus(_ SpanStatus, _ string)   {}
+func (*nopSpan) SpanContext() SpanContext           { return SpanContext{} }
+func (*nopSpan) SetAttributes(_ Tags)               {}
+func (*nopSpan) End()                               {}
