8fa99ea0ed78c3e90fbbb735efe95873afc15f94
mihir <mihir20121997@gmail.com>
2025-05-20 19:16:31 +0530

    feat: add DeleteKey function to JSON parser for key deletion

--- jsonparser/switcher.go
+++ jsonparser/switcher.go
@@ -15,9 +15,10 @@ const (
 
 // switcher is a JSONParser implementation that switches between different implementations based on configuration
 type switcher struct {
-	getterFn func() string
-	setterFn func() string
-	impls    map[string]JSONParser
+	getterFn  func() string
+	setterFn  func() string
+	deleterFn func() string
+	impls     map[string]JSONParser
 }
 
 // GetValue delegates to the configured getter implementation
@@ -70,6 +71,11 @@ func (s *switcher) SetString(data []byte, key, value string) ([]byte, error) {
 	return s.setter().SetString(data, key, value)
 }
 
+// DeleteKey delegates to the configured setter implementation
+func (s *switcher) DeleteKey(data []byte, key string) ([]byte, error) {
+	return s.deleter().DeleteKey(data, key)
+}
+
 // getter returns the configured getter implementation
 func (s *switcher) getter() JSONParser {
 	if impl, ok := s.impls[s.getterFn()]; ok {
@@ -86,75 +92,32 @@ func (s *switcher) setter() JSONParser {
 	return s.impls[DefaultLib]
 }
 
+// deleter returns the configured deleter implementation
+func (s *switcher) deleter() JSONParser {
+	if impl, ok := s.impls[s.deleterFn()]; ok {
+		return impl
+	}
+	return s.impls[DefaultLib]
+}
+
 // NewWithConfig returns a new JSONParser implementation based on the configuration
 func NewWithConfig(conf *config.Config) JSONParser {
-	getter := conf.GetReloadableStringVar(DefaultLib, "JSONParser.Library.Getter", "JSONParser.Library").Load
-	setter := conf.GetReloadableStringVar(DefaultLib, "JSONParser.Library.Setter", "JSONParser.Library").Load
-
+	getterFn := conf.GetReloadableStringVar(DefaultLib, "JSONParser.Library.Getter", "JSONParser.Library").Load
+	setterFn := conf.GetReloadableStringVar(DefaultLib, "JSONParser.Library.Setter", "JSONParser.Library").Load
+	deleterFn := conf.GetReloadableStringVar(DefaultLib, "JSONParser.Library.Deleter", "JSONParser.Library").Load
 	return &switcher{
 		impls: map[string]JSONParser{
 			TidwallLib: &tidwallJSONParser{},
 			GrafanaLib: &grafanaJSONParser{},
 		},
-		getterFn: getter,
-		setterFn: setter,
-	}
-}
-
-// NewGetterWithConfig returns a new JSONGetter implementation based on the configuration
-func NewGetterWithConfig(conf *config.Config) JSONGetter {
-	getter := conf.GetReloadableStringVar(DefaultLib, "JSONParser.Library.Getter", "JSONParser.Library").Load
-
-	return &switcher{
-		impls: map[string]JSONParser{
-			TidwallLib: &tidwallJSONParser{},
-			GrafanaLib: &grafanaJSONParser{},
-		},
-		getterFn: getter,
-		setterFn: func() string { return "" }, // Not used for getter-only
-	}
-}
-
-// NewSetterWithConfig returns a new JSONSetter implementation based on the configuration
-func NewSetterWithConfig(conf *config.Config) JSONSetter {
-	setter := conf.GetReloadableStringVar(DefaultLib, "JSONParser.Library.Setter", "JSONParser.Library").Load
-
-	return &switcher{
-		impls: map[string]JSONParser{
-			TidwallLib: &tidwallJSONParser{},
-			GrafanaLib: &grafanaJSONParser{},
-		},
-		getterFn: func() string { return "" }, // Not used for setter-only
-		setterFn: setter,
+		getterFn:  getterFn,
+		setterFn:  setterFn,
+		deleterFn: deleterFn,
 	}
 }
 
 // NewWithLibrary returns a new JSONParser implementation based on the specified library
 func NewWithLibrary(library string) JSONParser {
-	switch library {
-	case TidwallLib:
-		return &tidwallJSONParser{}
-	case GrafanaLib:
-		return &grafanaJSONParser{}
-	default:
-		return &tidwallJSONParser{}
-	}
-}
-
-// NewGetterWithLibrary returns a new JSONGetter implementation based on the specified library
-func NewGetterWithLibrary(library string) JSONGetter {
-	switch library {
-	case TidwallLib:
-		return &tidwallJSONParser{}
-	case GrafanaLib:
-		return &grafanaJSONParser{}
-	default:
-		return &tidwallJSONParser{}
-	}
-}
-
-// NewSetterWithLibrary returns a new JSONSetter implementation based on the specified library
-func NewSetterWithLibrary(library string) JSONSetter {
 	switch library {
 	case TidwallLib:
 		return &tidwallJSONParser{}
