723d58e5b28b08c9ddc151e4823f7a7fa2fb1288
Alexandros Milaios <alexmilaios@gmail.com>
2025-05-29 09:57:36 +0300

    chore: add selectConfig struct

--- filemanager/s3manager_switcher.go
+++ filemanager/s3manager_switcher.go
@@ -14,7 +14,7 @@ import (
 type S3Manager interface {
 	FileManager
 	Bucket() string
-	SelectObjects(ctx context.Context, sqlExpession, key string) (<-chan []byte, error)
+	SelectObjects(ctx context.Context, config SelectConfig) (<-chan []byte, error)
 }
 
 func NewS3Manager(conf *kitconfig.Config, config map[string]interface{}, log logger.Logger, defaultTimeout func() time.Duration) (S3Manager, error) {
@@ -95,8 +95,15 @@ func (s *switchingS3Manager) Bucket() string {
 	return s.getManager().Bucket()
 }
 
-func (s *switchingS3Manager) SelectObjects(ctx context.Context, sqlExpession, key string) (<-chan []byte, error) {
-	return s.getManager().SelectObjects(ctx, sqlExpession, key)
+type SelectConfig struct {
+	SQLExpression string
+	Key           string
+	OutputFormat  string
+	InputFormat   string
+}
+
+func (s *switchingS3Manager) SelectObjects(ctx context.Context, config SelectConfig) (<-chan []byte, error) {
+	return s.getManager().SelectObjects(ctx, config)
 }
 
 func (s *switchingS3Manager) getManager() S3Manager {
