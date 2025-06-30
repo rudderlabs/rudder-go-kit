833ebd9d2b0cceb59374c388f8dc4c8c56e76189
Francesco Casula <fra.casula@gmail.com>
2025-06-30 18:18:43 +0200

    fix: tests

--- stats/otel_test.go
+++ stats/otel_test.go
@@ -800,10 +800,11 @@ func TestPrometheusDuplicatedAttributes(t *testing.T) {
 	c.Set("RuntimeStats.enabled", false)
 	ctrl := gomock.NewController(t)
 	loggerSpy := mock_logger.NewMockLogger(ctrl)
-	loggerSpy.EXPECT().Infof(gomock.Any(), gomock.Any()).AnyTimes()
-	loggerSpy.EXPECT().Warnf(
-		"removing tag %q for measurement %q since it is a resource attribute",
-		"instanceName", "foo",
+	loggerSpy.EXPECT().Infon(gomock.Any(), gomock.Any()).AnyTimes()
+	loggerSpy.EXPECT().Warnn(
+		"removing tag for measurement since it is a resource attribute",
+		logger.NewStringField("key", "instanceName"),
+		logger.NewStringField("measurement", "foo"),
 	).Times(1)
 	loggerFactory := mock_logger.NewMockLogger(ctrl)
 	loggerFactory.EXPECT().Child(gomock.Any()).Times(1).Return(loggerSpy)
