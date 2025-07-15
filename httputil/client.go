a4a881c6bbc091fa20b5ff21e4d1697922a2960b
Mihir Gandhi <mihir20121997@gmail.com>
2025-07-15 14:26:59 +0530

    feat: add default HTTP client and options for custom transport and timeout (#854)

--- httputil/client.go
+++ httputil/client.go
@@ -4,10 +4,24 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
+
+	"github.com/rudderlabs/rudder-go-kit/config"
 )
 
 const (
 	headerXForwardedFor = "X-Forwarded-For"
+
+	DefaultMaxIdleConnsPerHost   = 10
+	DefaultMaxConnsPerHost       = 100
+	DefaultDisableKeepAlives     = false
+	DefaultForceHTTP2            = true
+	DefaultMaxIdleConns          = 100
+	DefaultIdleConnTimeout       = 90 * time.Second
+	DefaultTLSHandshakeTimeout   = 10 * time.Second
+	DefaultExpectContinueTimeout = 1 * time.Second
+	// DefaultRequestTimeout is the default timeout for HTTP requests for default HttpClient.
+	DefaultRequestTimeout = 30 * time.Second
 )
 
 // CloseResponse closes the response's body. But reads at least some of the body so if it's
@@ -29,4 +43,97 @@ func GetRequestIP(req *http.Request) string {
 	}
 
 	return strings.ReplaceAll(addresses[0], " ", "")
+}
+
+func DefaultTransport() *http.Transport {
+	tr := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		ForceAttemptHTTP2:     DefaultForceHTTP2,
+		MaxIdleConns:          DefaultMaxIdleConns,
+		IdleConnTimeout:       DefaultIdleConnTimeout,
+		TLSHandshakeTimeout:   DefaultTLSHandshakeTimeout,
+		ExpectContinueTimeout: DefaultExpectContinueTimeout,
+		DisableKeepAlives:     DefaultDisableKeepAlives,
+		MaxConnsPerHost:       DefaultMaxConnsPerHost,
+		MaxIdleConnsPerHost:   DefaultMaxIdleConnsPerHost,
+	}
+	return tr
+}
+
+type HttpClientOptions func(*http.Client)
+
+// DefaultHttpClient returns a default HTTP client with a custom transport configuration.
+// It disables keep-alives, sets max connections per host, and configures idle connection timeout.
+// This is useful for clients that need to make many short-lived requests without reusing connections.
+// It also sets a default timeout of 30 seconds.
+func DefaultHttpClient() *http.Client {
+	return &http.Client{
+		Transport: DefaultTransport(),
+		Timeout:   DefaultRequestTimeout,
+	}
+}
+
+func WithTimeout(timeout time.Duration) HttpClientOptions {
+	return func(client *http.Client) {
+		if client == nil {
+			client = DefaultHttpClient()
+		}
+		client.Timeout = timeout
+	}
+}
+
+// WithTransport returns a HttpClientOptions that sets a custom transport for the HTTP client.
+func WithTransport(transport *http.Transport) HttpClientOptions {
+	return func(client *http.Client) {
+		if client == nil {
+			client = DefaultHttpClient()
+		}
+		client.Transport = transport
+	}
+}
+
+// WithConfig returns a HttpClientOptions that sets a custom configuration for the HTTP client.
+func WithConfig(conf *config.Config, prefix ...string) HttpClientOptions {
+	return func(client *http.Client) {
+		if client == nil {
+			client = DefaultHttpClient()
+		}
+		tr := &http.Transport{
+			Proxy:                 http.ProxyFromEnvironment,
+			ForceAttemptHTTP2:     conf.GetBoolVar(DefaultForceHTTP2, getConfigKeys("ForceHTTP2", prefix...)...),
+			MaxIdleConns:          conf.GetIntVar(DefaultMaxIdleConns, 1, getConfigKeys("MaxIdleConns", prefix...)...),
+			IdleConnTimeout:       conf.GetDurationVar(int64(DefaultIdleConnTimeout/time.Second), time.Second, getConfigKeys("IdleConnTimeout", prefix...)...),
+			TLSHandshakeTimeout:   conf.GetDurationVar(int64(DefaultTLSHandshakeTimeout/time.Second), time.Second, getConfigKeys("TLSHandshakeTimeout", prefix...)...),
+			ExpectContinueTimeout: conf.GetDurationVar(int64(DefaultExpectContinueTimeout/time.Second), time.Second, getConfigKeys("ExpectContinueTimeout", prefix...)...),
+			DisableKeepAlives:     conf.GetBoolVar(DefaultDisableKeepAlives, getConfigKeys("DisableKeepAlives", prefix...)...),
+			MaxConnsPerHost:       conf.GetIntVar(DefaultMaxConnsPerHost, 1, getConfigKeys("MaxConnsPerHost", prefix...)...),
+			MaxIdleConnsPerHost:   conf.GetIntVar(DefaultMaxIdleConnsPerHost, 1, getConfigKeys("MaxIdleConnsPerHost", prefix...)...),
+		}
+		client.Transport = tr
+	}
+}
+
+func getConfigKeys(key string, prefix ...string) []string {
+	res := make([]string, 0, len(prefix)+1)
+	for _, p := range prefix {
+		res = append(res, p+"."+key)
+	}
+	res = append(res, "DefaultHttpClient."+key)
+	return res
+}
+
+// NewHttpClient creates a configured HTTP client with customizable options.
+// It initializes a client with DefaultHttpClient settings and applies functional options.
+// Parameters:
+//   - options: Variadic list of HttpClientOptions functions to customize client behavior
+//
+// Retur
+// Returns:
+//   - *http.Client configured with specified options
+func NewHttpClient(options ...HttpClientOptions) *http.Client {
+	client := DefaultHttpClient()
+	for _, option := range options {
+		option(client)
+	}
+	return client
 }
--- httputil/client_test.go
+++ httputil/client_test.go
@@ -3,6 +3,9 @@ package httputil_test
 import (
 	"net/http"
 	"testing"
+	"time"
+
+	"github.com/rudderlabs/rudder-go-kit/config"
 
 	"github.com/stretchr/testify/require"
 
@@ -45,4 +48,172 @@ func TestGetRequestIP(t *testing.T) {
 			require.Equal(t, testCase.expectedResult, httputil.GetRequestIP(req))
 		})
 	}
+}
+
+func TestDefaultHttpClient(t *testing.T) {
+	client := httputil.DefaultHttpClient()
+	require.NotNil(t, client)
+	require.Equal(t, httputil.DefaultRequestTimeout, client.Timeout)
+
+	transport, ok := client.Transport.(*http.Transport)
+	require.True(t, ok)
+	require.False(t, transport.DisableKeepAlives)
+	require.Equal(t, httputil.DefaultMaxConnsPerHost, transport.MaxConnsPerHost)
+	require.Equal(t, httputil.DefaultMaxIdleConnsPerHost, transport.MaxIdleConnsPerHost)
+}
+
+func TestNewHttpClientWithOptions(t *testing.T) {
+	customTransport := &http.Transport{
+		DisableKeepAlives: false,
+		Proxy:             http.ProxyURL(nil),
+	}
+	testCases := []struct {
+		name          string
+		options       []httputil.HttpClientOptions
+		expectedCheck func(*testing.T, *http.Client)
+	}{
+		{
+			name:    "No options",
+			options: nil,
+			expectedCheck: func(t *testing.T, client *http.Client) {
+				require.Equal(t, httputil.DefaultRequestTimeout, client.Timeout)
+				transport, ok := client.Transport.(*http.Transport)
+				require.True(t, ok)
+				require.False(t, transport.DisableKeepAlives)
+			},
+		},
+		{
+			name:    "WithTimeout",
+			options: []httputil.HttpClientOptions{httputil.WithTimeout(5 * time.Second)},
+			expectedCheck: func(t *testing.T, client *http.Client) {
+				require.Equal(t, 5*time.Second, client.Timeout)
+				transport, ok := client.Transport.(*http.Transport)
+				require.True(t, ok)
+				require.False(t, transport.DisableKeepAlives)
+			},
+		},
+		{
+			name:    "WithTransport",
+			options: []httputil.HttpClientOptions{httputil.WithTransport(customTransport)},
+			expectedCheck: func(t *testing.T, client *http.Client) {
+				require.NotNil(t, client)
+				require.Equal(t, httputil.DefaultRequestTimeout, client.Timeout)
+				require.Same(t, customTransport, client.Transport)
+			},
+		},
+		{
+			name: "Multiple options",
+			options: []httputil.HttpClientOptions{
+				httputil.WithTimeout(15 * time.Second),
+				httputil.WithTransport(customTransport),
+			},
+			expectedCheck: func(t *testing.T, client *http.Client) {
+				require.NotNil(t, client)
+				require.Equal(t, 15*time.Second, client.Timeout)
+				require.Same(t, customTransport, client.Transport)
+			},
+		},
+		{
+			name: "WithConfig",
+			options: []httputil.HttpClientOptions{
+				httputil.WithConfig(config.New()),
+			},
+			expectedCheck: func(t *testing.T, client *http.Client) {
+				require.NotNil(t, client)
+				require.Equal(t, httputil.DefaultRequestTimeout, client.Timeout)
+				transport, ok := client.Transport.(*http.Transport)
+				require.True(t, ok)
+				require.False(t, transport.DisableKeepAlives)
+				require.Equal(t, httputil.DefaultMaxConnsPerHost, transport.MaxConnsPerHost)
+				require.Equal(t, httputil.DefaultMaxIdleConnsPerHost, transport.MaxIdleConnsPerHost)
+				require.Equal(t, httputil.DefaultIdleConnTimeout, transport.IdleConnTimeout)
+				require.Equal(t, httputil.DefaultTLSHandshakeTimeout, transport.TLSHandshakeTimeout)
+				require.Equal(t, httputil.DefaultExpectContinueTimeout, transport.ExpectContinueTimeout)
+				require.Equal(t, httputil.DefaultForceHTTP2, transport.ForceAttemptHTTP2)
+			},
+		},
+		{
+			name: "WithConfig and without prefix",
+			options: []httputil.HttpClientOptions{
+				httputil.WithConfig(func() *config.Config {
+					conf := config.New()
+					conf.Set("DefaultHttpClient.ForceHTTP2", false)
+					return conf
+				}()),
+			},
+			expectedCheck: func(t *testing.T, client *http.Client) {
+				require.NotNil(t, client)
+				require.Equal(t, httputil.DefaultRequestTimeout, client.Timeout)
+				transport, ok := client.Transport.(*http.Transport)
+				require.True(t, ok)
+				require.False(t, transport.DisableKeepAlives)
+				require.Equal(t, httputil.DefaultMaxConnsPerHost, transport.MaxConnsPerHost)
+				require.Equal(t, httputil.DefaultMaxIdleConnsPerHost, transport.MaxIdleConnsPerHost)
+				require.Equal(t, httputil.DefaultIdleConnTimeout, transport.IdleConnTimeout)
+				require.Equal(t, httputil.DefaultTLSHandshakeTimeout, transport.TLSHandshakeTimeout)
+				require.Equal(t, httputil.DefaultExpectContinueTimeout, transport.ExpectContinueTimeout)
+				require.False(t, transport.ForceAttemptHTTP2)
+			},
+		},
+		{
+			name: "WithConfig and with prefix",
+			options: []httputil.HttpClientOptions{
+				httputil.WithConfig(func() *config.Config {
+					conf := config.New()
+					conf.Set("router.httpclient.ForceHTTP2", false)
+					conf.Set("DefaultHttpClient.ForceHTTP2", true)
+					return conf
+				}(), "router.httpclient"),
+			},
+			expectedCheck: func(t *testing.T, client *http.Client) {
+				require.NotNil(t, client)
+				require.Equal(t, httputil.DefaultRequestTimeout, client.Timeout)
+				transport, ok := client.Transport.(*http.Transport)
+				require.True(t, ok)
+				require.False(t, transport.DisableKeepAlives)
+				require.Equal(t, httputil.DefaultMaxConnsPerHost, transport.MaxConnsPerHost)
+				require.Equal(t, httputil.DefaultMaxIdleConnsPerHost, transport.MaxIdleConnsPerHost)
+				require.Equal(t, httputil.DefaultIdleConnTimeout, transport.IdleConnTimeout)
+				require.Equal(t, httputil.DefaultTLSHandshakeTimeout, transport.TLSHandshakeTimeout)
+				require.Equal(t, httputil.DefaultExpectContinueTimeout, transport.ExpectContinueTimeout)
+				require.False(t, transport.ForceAttemptHTTP2)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			client := httputil.NewHttpClient(tc.options...)
+			tc.expectedCheck(t, client)
+		})
+	}
+}
+
+func ExampleNewHttpClient() {
+	// no need to use .Clone() since a new transport is built each time
+	transport := httputil.DefaultTransport()
+	transport.ForceAttemptHTTP2 = false
+	client := httputil.NewHttpClient(httputil.WithTransport(transport))
+	req, err := http.NewRequest("GET", "https://example.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	_ = resp.Body.Close()
+}
+
+func ExampleDefaultHttpClient() {
+	client := httputil.DefaultHttpClient()
+	req, err := http.NewRequest("GET", "https://example.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	_ = resp.Body.Close()
 }
