b16654463be6a685ad5fd5e188bb8b34488fe1d9
Francesco Casula <fracasula@users.noreply.github.com>
2025-07-18 07:54:33 +0200

    feat: new int slice field (#866)

--- logger/field.go
+++ logger/field.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"strconv"
+	"strings"
 	"time"
 
 	"go.uber.org/zap"
@@ -106,6 +108,27 @@ func NewDurationField(key string, v time.Duration) Field {
 
 func NewErrorField(v error) Field {
 	return Field{name: "error", error: v, fieldType: ErrorType}
+}
+
+type intT interface {
+	uint | uint8 | uint16 | uint32 | uint64 | int | int8 | int16 | int32 | int64
+}
+
+func NewIntSliceField[K intT](key string, v []K) Field {
+	var value strings.Builder
+	for i, vv := range v {
+		if i > 0 {
+			value.WriteString(",")
+		}
+		// Handle uint64 separately to avoid truncation
+		switch any(vv).(type) {
+		case uint64:
+			value.WriteString(strconv.FormatUint(uint64(vv), 10))
+		default:
+			value.WriteString(strconv.FormatInt(int64(vv), 10))
+		}
+	}
+	return Field{name: key, string: value.String(), fieldType: StringType}
 }
 
 // Expand is useful if you want to use the type Field with the sugared logger
