a1f12a1af510100fc2f54b31d01e898674ba8c8f
Dilip Kola <dilipkola@rudderstack.com>
2025-08-18 11:40:06 +0530

    fix: remove redudent max function

--- lcs/lcs.go
+++ lcs/lcs.go
@@ -119,11 +119,3 @@ func longestCommonSubsequence[T comparable](seq1, seq2 []T) int {
 
 	return dp[m][n]
 }
-
-// max returns the maximum of two integers
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
