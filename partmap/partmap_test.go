9ed27e3d43380c32d47a4c4c4771f3b103a19afc
Aris Tzoumas <atzoumas@rudderstack.com>
2025-11-18 09:42:05 +0200

    chore: user require instead of assert
    🔒 Scanned for secrets using gitleaks 8.28.0

--- partmap/partmap_test.go
+++ partmap/partmap_test.go
@@ -3,7 +3,7 @@ package partmap_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/rudderlabs/rudder-go-kit/partmap"
 )
@@ -17,53 +17,53 @@ func TestMurmur3Partition32(t *testing.T) {
 			t.Run("2 partitions", func(t *testing.T) {
 				numPartitions := uint32(2)
 				partitionIdx, startsAt := partmap.Murmur3Partition32(key, numPartitions)
-				assert.EqualValues(t, 1, int(partitionIdx), "invalid partition index")
-				assert.EqualValues(t, 2147483648, int(startsAt), "invalid startsAt")
+				require.EqualValues(t, 1, int(partitionIdx), "invalid partition index")
+				require.EqualValues(t, 2147483648, int(startsAt), "invalid startsAt")
 
 				t.Run("empty key", func(t *testing.T) {
 					partitionIdx, startsAt := partmap.Murmur3Partition32(emptyKey, numPartitions)
-					assert.EqualValues(t, 0, int(partitionIdx), "partition index")
-					assert.EqualValues(t, 0, int(startsAt), "invalid startsAt")
+					require.EqualValues(t, 0, int(partitionIdx), "partition index")
+					require.EqualValues(t, 0, int(startsAt), "invalid startsAt")
 				})
 				t.Run("upper range", func(t *testing.T) {
 					partitionIdx, startsAt := partmap.Murmur3Partition32("1", numPartitions)
-					assert.EqualValues(t, 1, int(partitionIdx), "upper range key should map to highest partition")
-					assert.EqualValues(t, 2147483648, int(startsAt), "invalid startsAt")
+					require.EqualValues(t, 1, int(partitionIdx), "upper range key should map to highest partition")
+					require.EqualValues(t, 2147483648, int(startsAt), "invalid startsAt")
 				})
 			})
 
 			t.Run("16 partitions", func(t *testing.T) {
 				numPartitions := uint32(16)
 				partitionIdx, startsAt := partmap.Murmur3Partition32(key, numPartitions)
-				assert.EqualValues(t, 12, int(partitionIdx), "partition index")
-				assert.EqualValues(t, 3221225472, int(startsAt), "invalid startsAt")
+				require.EqualValues(t, 12, int(partitionIdx), "partition index")
+				require.EqualValues(t, 3221225472, int(startsAt), "invalid startsAt")
 				t.Run("empty key", func(t *testing.T) {
 					partitionIdx, startsAt := partmap.Murmur3Partition32(emptyKey, numPartitions)
-					assert.EqualValues(t, 0, int(partitionIdx), "partition index")
-					assert.EqualValues(t, 0, int(startsAt), "invalid startsAt")
+					require.EqualValues(t, 0, int(partitionIdx), "partition index")
+					require.EqualValues(t, 0, int(startsAt), "invalid startsAt")
 				})
 				t.Run("upper range", func(t *testing.T) {
 					partitionIdx, startsAt := partmap.Murmur3Partition32("12", numPartitions)
-					assert.EqualValues(t, 15, int(partitionIdx), "upper range key should map to highest partition")
-					assert.EqualValues(t, 4026531840, int(startsAt), "invalid startsAt")
+					require.EqualValues(t, 15, int(partitionIdx), "upper range key should map to highest partition")
+					require.EqualValues(t, 4026531840, int(startsAt), "invalid startsAt")
 				})
 			})
 
 			t.Run("64 partitions", func(t *testing.T) {
 				numPartitions := uint32(64)
 				partitionIdx, startsAt := partmap.Murmur3Partition32(key, numPartitions)
-				assert.EqualValues(t, 49, int(partitionIdx), "partition index")
-				assert.EqualValues(t, 3288334336, int(startsAt), "invalid startsAt")
+				require.EqualValues(t, 49, int(partitionIdx), "partition index")
+				require.EqualValues(t, 3288334336, int(startsAt), "invalid startsAt")
 				t.Run("empty key", func(t *testing.T) {
 					partitionIdx, startsAt := partmap.Murmur3Partition32(emptyKey, numPartitions)
-					assert.EqualValues(t, 0, int(partitionIdx), "partition index")
-					assert.EqualValues(t, 0, int(startsAt), "invalid startsAt")
+					require.EqualValues(t, 0, int(partitionIdx), "partition index")
+					require.EqualValues(t, 0, int(startsAt), "invalid startsAt")
 				})
 
 				t.Run("upper range", func(t *testing.T) {
 					partitionIdx, startsAt := partmap.Murmur3Partition32("49", numPartitions)
-					assert.EqualValues(t, 63, int(partitionIdx), "upper range key should map to highest partition")
-					assert.EqualValues(t, 4227858432, int(startsAt), "invalid startsAt")
+					require.EqualValues(t, 63, int(partitionIdx), "upper range key should map to highest partition")
+					require.EqualValues(t, 4227858432, int(startsAt), "invalid startsAt")
 				})
 			})
 		})
