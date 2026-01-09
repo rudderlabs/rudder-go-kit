9d774be5f57e086e81553a321d38b2eeeda46f95
mihir <mihir20121997@gmail.com>
2026-01-10 02:54:16 +0530

    chore: implement ETCD watch functionality with test cases
    🔒 Scanned for secrets using gitleaks 8.28.0

--- testhelper/docker/resource/etcd/etcd_test.go
+++ testhelper/docker/resource/etcd/etcd_test.go
@@ -3,6 +3,7 @@ package etcd
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/ory/dockertest/v3"
 	"github.com/stretchr/testify/require"
@@ -32,4 +33,32 @@ func TestETCD(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, getResp.Kvs, 1)
 	require.Equal(t, string(getResp.Kvs[0].Value), dummyValue)
+}
+
+func TestETCD_Watch(t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	etcdRes, err := Setup(pool, t)
+	require.NoError(t, err)
+
+	watchChan := etcdRes.Client.Watch(context.Background(), dummyKey)
+
+	_, err = etcdRes.Client.Put(context.Background(), dummyKey, dummyValue) // put value in to dummyKey
+	require.NoError(t, err)
+
+	watchResp := <-watchChan
+	require.Len(t, watchResp.Events, 1)
+	require.Equal(t, string(watchResp.Events[0].Kv.Value), dummyValue)
+
+	err = pool.Purge(etcdRes.Resource)
+	require.NoError(t, err)
+
+	select {
+	case e, ok := <-watchChan:
+		require.Error(t, e.Err())
+		require.False(t, ok)
+	case <-time.After(5 * time.Second):
+		require.Fail(t, "watch channel is not closed")
+	}
 }
