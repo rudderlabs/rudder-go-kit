0eec98018108b9c2de6918b78b557cc97673c320
Aris Tzoumas <atzoumas@rudderstack.com>
2026-02-10 19:28:39 +0200

    chore: use ecr registry as registry mirror (#1025)

--- testhelper/docker/resource/registry/registry.go
+++ testhelper/docker/resource/registry/registry.go
@@ -3,14 +3,30 @@ package registry
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	dc "github.com/ory/dockertest/v3/docker"
 )
 
-// ImagePath returns the full image path based on whether a registry mirror is configured
+// ImagePath returns the full image path based on whether a dockerhub registry mirror is configured
 func ImagePath(image string) string {
-	mirrorURL := os.Getenv("DOCKER_REGISTRY_MIRROR")
-	if mirrorURL != "" {
+	mirrorURL := os.Getenv("DOCKERHUB_REGISTRY_MIRROR")
+
+	hasRegistry := func() bool { // If the image already has a registry, we don't need to prepend the mirror URL
+		parts := strings.Split(image, "/")
+		if len(parts) == 0 {
+			return false
+		}
+		host := parts[0]
+		return strings.Contains(host, ".") ||
+			strings.Contains(host, ":") ||
+			host == "localhost"
+	}()
+	if mirrorURL != "" && !hasRegistry {
+		needsLibrary := !strings.Contains(image, "/")
+		if needsLibrary {
+			image = "library/" + image
+		}
 		return fmt.Sprintf("%s/%s", mirrorURL, image)
 	}
 	return image
@@ -18,8 +34,8 @@ func ImagePath(image string) string {
 
 // AuthConfiguration returns the authentication configuration for the registry
 func AuthConfiguration() dc.AuthConfiguration {
-	username := os.Getenv("DOCKER_REGISTRY_MIRROR_USERNAME")
-	password := os.Getenv("DOCKER_REGISTRY_MIRROR_PASSWORD")
+	username := os.Getenv("DOCKERHUB_REGISTRY_MIRROR_USERNAME")
+	password := os.Getenv("DOCKERHUB_REGISTRY_MIRROR_PASSWORD")
 
 	if username == "" && password == "" {
 		return dc.AuthConfiguration{}
